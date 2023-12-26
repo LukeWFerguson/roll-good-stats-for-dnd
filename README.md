b0a4936e24a94eb7b80a2cb164ef9b970fcb1973
LukeWFerguson <lukewferguson@gmail.com>
2023-12-26 12:22:01 -0600

    17 or higher with 3 18s...

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # roll-good-stats-for-dnd
 Rolls good stats for your abilities
+
+## How to run
+* Install Node.js if you haven't already. You can download it from the official website: Node.js.
+* Open a terminal or command prompt.
+* Navigate to the directory where the script is saved using the cd command.
+* Run the script with the command: node roll_good_stats_for_dnd.js
---
+++ roll_good_stats_for_dnd.js
@@ -0,0 +1,46 @@
+function rollDice() {
+  // Roll 4 six-sided dice
+  const diceRolls = Array.from({ length: 4 }, () => Math.floor(Math.random() * 6) + 1);
+
+  // Sum the three highest rolls (dropping the lowest)
+  const sum = diceRolls.sort((a, b) => a - b).slice(1).reduce((acc, val) => acc + val, 0);
+
+  return sum;
+}
+
+let numberOf18s = 0;
+let rollsCount = 0;
+
+while (numberOf18s < 3) {
+  rollsCount = 0;
+  let currentRolls = [];
+
+  while (rollsCount < 6) {
+    const result = rollDice();
+    rollsCount++;
+
+    // Check if the lowest roll is 17 or higher
+    const minRoll = Math.min(...Array.from({ length: 4 }, (_, i) => result));
+    if (minRoll >= 17) {
+      currentRolls.push(`Roll ${rollsCount}: ${result}`);
+      
+      if (result === 18) {
+        numberOf18s++;
+      }
+    } else {
+      // If the condition is not met, restart the loop
+      rollsCount = 0;
+      currentRolls = [];
+      numberOf18s = 0;
+      continue;
+    }
+  }
+
+  // Check if the set of rolls has at least 3 instances of 18
+  if (numberOf18s === 3) {
+    currentRolls.forEach(roll => console.log(roll));
+    console.log("----");
+  }
+}
+
+console.log("Successfully rolled 3 instances of 18 within 6 rolls.");
